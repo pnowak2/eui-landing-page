00560ed6b3011db5e448c4f27e1b269ff42615c7
Piotr Nowak <p.nowak2@gmail.com>
2017-09-18 20:58:01 +0200

    added autoprefixer.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
@@ -17,11 +18,15 @@ var banner = ['/*!\n',
 ].join('');
 
 // Compiles SCSS files from /scss into /css
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src('scss/eui-landing-page.scss')
     .pipe(sass())
     .pipe(header(banner, {
       pkg: pkg
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
     }))
     .pipe(gulp.dest('css'))
     .pipe(browserSync.reload({
@@ -30,7 +35,7 @@ gulp.task('sass', function() {
 });
 
 // Minify compiled CSS
-gulp.task('minify-css', ['sass'], function() {
+gulp.task('minify-css', ['sass'], function () {
   return gulp.src('css/eui-landing-page.css')
     .pipe(cleanCSS({
       compatibility: 'ie8'
@@ -45,7 +50,7 @@ gulp.task('minify-css', ['sass'], function() {
 });
 
 // Minify custom JS
-gulp.task('minify-js', function() {
+gulp.task('minify-js', function () {
   return gulp.src('js/eui-landing-page.js')
     .pipe(uglify())
     .pipe(header(banner, {
@@ -62,13 +67,13 @@ gulp.task('minify-js', function() {
 
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
-gulp.task('copy', function() {
+gulp.task('copy', function () {
   gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
+    'node_modules/bootstrap/dist/**/*',
+    '!**/npm.js',
+    '!**/bootstrap-theme.*',
+    '!**/*.map'
+  ])
     .pipe(gulp.dest('vendor/bootstrap'))
 
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
@@ -87,13 +92,13 @@ gulp.task('copy', function() {
     .pipe(gulp.dest('vendor/jquery-easing'))
 
   gulp.src([
-      'node_modules/font-awesome/**',
-      '!node_modules/font-awesome/**/*.map',
-      '!node_modules/font-awesome/.npmignore',
-      '!node_modules/font-awesome/*.txt',
-      '!node_modules/font-awesome/*.md',
-      '!node_modules/font-awesome/*.json'
-    ])
+    'node_modules/font-awesome/**',
+    '!node_modules/font-awesome/**/*.map',
+    '!node_modules/font-awesome/.npmignore',
+    '!node_modules/font-awesome/*.txt',
+    '!node_modules/font-awesome/*.md',
+    '!node_modules/font-awesome/*.json'
+  ])
     .pipe(gulp.dest('vendor/font-awesome'))
 })
 
@@ -101,7 +106,7 @@ gulp.task('copy', function() {
 gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync.init({
     server: {
       baseDir: ''
@@ -110,7 +115,7 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
+gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function () {
   gulp.watch('scss/*.scss', ['sass']);
   gulp.watch('css/*.css', ['minify-css']);
   gulp.watch('js/*.js', ['minify-js']);
